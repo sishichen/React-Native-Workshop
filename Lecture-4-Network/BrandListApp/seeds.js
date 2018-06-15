d4df6b3026d9f538c182cb4c4b80ad17de8590b8
sishichen <sishi.chen@honestbee.com>
2018-06-15 12:35:09 +0800

    Lecture 4 add network part and sample code (#5)
    * Lecture 4 add network part and sample code
    
    * Lecture 4 network finish

---
+++ Lecture-4-Network/BrandListApp/seeds.js
@@ -0,0 +1,700 @@
+export const test = [];
+
+export const mockBrands = [
+  {
+    id: 464,
+    name: 'Standing Sushi Bar',
+    slug: 'standing-sushi-bar',
+    description: null,
+    about: null,
+    imageUrl: 'https://assets-staging.honestbee.com/headers/images/360x120/standing-sushi-bar.png',
+    productsImageUrl: '',
+    brandColor: '',
+    currency: 'SGD',
+    sameStorePrice: null,
+    brandType: 'regular',
+    promotionText: '',
+    parentBrandId: null,
+    productsCount: 55,
+    storeId: 613,
+    serviceType: 'food',
+    freeDeliveryEligible: true,
+    itemReplacementOptions: [
+      'find_best_match',
+      'pick_specific_replacement',
+      'do_not_replace'
+    ],
+    estimatedDeliveryTime: 40,
+    closed: true,
+    opensAt: '2018-05-31T00:00:00.000+08:00',
+    tags: [],
+    minimumSpend: '15.0',
+    minimumSpendExtraFee: '30.99',
+    deliveryTypes: [
+      'asap',
+      'scheduled'
+    ],
+    shippingModes: [
+      'regular'
+    ],
+    cashbackAmount: 0,
+    reservedTags: [
+      {
+        id: 101,
+        title: '',
+        key: 'habitat',
+        imageUrl: null
+      },
+      {
+        id: 95,
+        title: 'Eat More, Earn More',
+        key: 'cashback',
+        imageUrl: 'https://assets.honestbee.com/food/tags/foodtag-sumo6.jpg'
+      }
+    ],
+    minimumOrderFreeDelivery: '600.0',
+    defaultDeliveryFee: '10.0',
+    defaultConciergeFee: '0.0',
+    pricingType: 'no_pricing_policy',
+    brandTraits: []
+  },
+  {
+    id: 917,
+    name: 'SaladStop!',
+    slug: 'saladstop',
+    description: null,
+    about: 'TEXT',
+    imageUrl: 'https://assets-staging.honestbee.com/headers/images/360x120/saladstop.png',
+    productsImageUrl: '',
+    brandColor: '',
+    currency: 'SGD',
+    sameStorePrice: null,
+    brandType: 'regular',
+    promotionText: '',
+    parentBrandId: null,
+    productsCount: 55,
+    storeId: 1358,
+    serviceType: 'food',
+    freeDeliveryEligible: true,
+    itemReplacementOptions: [
+      'find_best_match',
+      'pick_specific_replacement',
+      'do_not_replace'
+    ],
+    estimatedDeliveryTime: 40,
+    closed: true,
+    opensAt: '2018-05-31T00:00:00.000+08:00',
+    tags: [],
+    minimumSpend: '15.0',
+    minimumSpendExtraFee: '30.99',
+    deliveryTypes: [
+      'asap',
+      'scheduled'
+    ],
+    shippingModes: [
+      'regular'
+    ],
+    cashbackAmount: 6.66,
+    reservedTags: [
+      {
+        id: 101,
+        title: '',
+        key: 'habitat',
+        imageUrl: null
+      },
+      {
+        id: 95,
+        title: 'Eat More, Earn More',
+        key: 'cashback',
+        imageUrl: 'https://assets.honestbee.com/food/tags/foodtag-sumo6.jpg'
+      }
+    ],
+    minimumOrderFreeDelivery: '0.0',
+    defaultDeliveryFee: '0.0',
+    defaultConciergeFee: '0.0',
+    pricingType: 'no_pricing_policy',
+    brandTraits: []
+  },
+  {
+    id: 476,
+    name: 'J3 Juice',
+    slug: 'j3-juice',
+    description: 'Recharge your body and get to juicing',
+    about: 'Recharge your body and get to juicing! Sip on 100% cold-pressed with no sugar, water, or additives added to the juices. One step closer to a healthier lifestyle.',
+    imageUrl: 'https://assets-staging.honestbee.com/headers/images/360x120/j3-juice.png',
+    productsImageUrl: '',
+    brandColor: '',
+    currency: 'SGD',
+    sameStorePrice: null,
+    brandType: 'regular',
+    promotionText: '',
+    parentBrandId: null,
+    productsCount: 24,
+    storeId: 625,
+    serviceType: 'food',
+    freeDeliveryEligible: true,
+    itemReplacementOptions: [
+      'find_best_match',
+      'pick_specific_replacement',
+      'do_not_replace'
+    ],
+    estimatedDeliveryTime: 40,
+    closed: true,
+    opensAt: '2018-05-31T00:00:00.000+08:00',
+    tags: [
+      {
+        id: 76,
+        title: '',
+        key: 'Juice',
+        imageUrl: 'https://assets.honestbee.com/food/tags/juice.jpg'
+      }
+    ],
+    minimumSpend: '15.0',
+    minimumSpendExtraFee: '30.99',
+    deliveryTypes: [
+      'asap'
+    ],
+    shippingModes: [
+      'regular'
+    ],
+    cashbackAmount: 6,
+    reservedTags: [
+      {
+        id: 101,
+        title: '',
+        key: 'habitat',
+        imageUrl: null
+      },
+      {
+        id: 95,
+        title: 'Eat More, Earn More',
+        key: 'cashback',
+        imageUrl: 'https://assets.honestbee.com/food/tags/foodtag-sumo6.jpg'
+      }
+    ],
+    minimumOrderFreeDelivery: '0.0',
+    defaultDeliveryFee: '0.0',
+    defaultConciergeFee: '5.0',
+    pricingType: 'no_pricing_policy',
+    brandTraits: []
+  },
+  {
+    id: 7749,
+    name: 'Brand4Test',
+    slug: 'brandtest',
+    description: 'used to check food+',
+    about: null,
+    imageUrl: 'https://assets-staging.honestbee.com/headers/images/360x120/brandtest.png',
+    productsImageUrl: 'https://assets-staging.honestbee.com/headers/images/360x120/product-list-brandtest.png',
+    brandColor: '',
+    currency: 'SGD',
+    sameStorePrice: true,
+    brandType: 'regular',
+    promotionText: '',
+    parentBrandId: null,
+    productsCount: 100,
+    storeId: 10704,
+    serviceType: 'food',
+    freeDeliveryEligible: true,
+    itemReplacementOptions: [
+      'find_best_match',
+      'pick_specific_replacement',
+      'do_not_replace'
+    ],
+    estimatedDeliveryTime: 20,
+    closed: true,
+    opensAt: '2018-05-31T09:00:00.000+08:00',
+    tags: [],
+    minimumSpend: '15.0',
+    minimumSpendExtraFee: '30.99',
+    deliveryTypes: [
+      'asap',
+      'scheduled'
+    ],
+    shippingModes: [
+      'regular'
+    ],
+    cashbackAmount: 0,
+    reservedTags: [
+      {
+        id: 101,
+        title: '',
+        key: 'habitat',
+        imageUrl: null
+      },
+      {
+        id: 95,
+        title: 'Eat More, Earn More',
+        key: 'cashback',
+        imageUrl: 'https://assets.honestbee.com/food/tags/foodtag-sumo6.jpg'
+      }
+    ],
+    minimumOrderFreeDelivery: '0.0',
+    defaultDeliveryFee: null,
+    defaultConciergeFee: null,
+    pricingType: 'same_price_as_store',
+    brandTraits: [
+      {
+        slug: 'food',
+        name: 'Food',
+        header: 'Food',
+        imageUrlBasename: ''
+      }
+    ]
+  },
+  {
+    id: 509,
+    name: 'Folks Collective',
+    slug: 'folks-collective',
+    description: 'Good\r\nAwesome\r\n!!!!!!!!!!!!!!',
+    about: null,
+    imageUrl: 'https://assets-staging.honestbee.com/headers/images/360x120/folks-collective.png',
+    productsImageUrl: '',
+    brandColor: '',
+    currency: 'SGD',
+    sameStorePrice: null,
+    brandType: 'regular',
+    promotionText: '',
+    parentBrandId: null,
+    productsCount: 119,
+    storeId: 712,
+    serviceType: 'food',
+    freeDeliveryEligible: true,
+    itemReplacementOptions: [
+      'find_best_match',
+      'pick_specific_replacement',
+      'do_not_replace'
+    ],
+    estimatedDeliveryTime: 40,
+    closed: true,
+    opensAt: '2018-05-31T09:00:00.000+08:00',
+    tags: [],
+    minimumSpend: '15.0',
+    minimumSpendExtraFee: '30.99',
+    deliveryTypes: [
+      'asap',
+      'scheduled'
+    ],
+    shippingModes: [
+      'regular'
+    ],
+    cashbackAmount: 0,
+    reservedTags: [
+      {
+        id: 101,
+        title: '',
+        key: 'habitat',
+        imageUrl: null
+      },
+      {
+        id: 95,
+        title: 'Eat More, Earn More',
+        key: 'cashback',
+        imageUrl: 'https://assets.honestbee.com/food/tags/foodtag-sumo6.jpg'
+      }
+    ],
+    minimumOrderFreeDelivery: '0.0',
+    defaultDeliveryFee: '0.0',
+    defaultConciergeFee: '0.0',
+    pricingType: 'no_pricing_policy',
+    brandTraits: []
+  },
+  {
+    id: 1106,
+    name: 'LAAF COFFEE & BISTRO',
+    slug: 'laaf-coffee--bistro',
+    description: null,
+    about: null,
+    imageUrl: 'https://assets-staging.honestbee.com/headers/images/360x120/laaf-coffee--bistro.png',
+    productsImageUrl: '',
+    brandColor: '',
+    currency: 'SGD',
+    sameStorePrice: null,
+    brandType: 'regular',
+    promotionText: '',
+    parentBrandId: null,
+    productsCount: 68,
+    storeId: 1623,
+    serviceType: 'food',
+    freeDeliveryEligible: true,
+    itemReplacementOptions: [
+      'find_best_match',
+      'pick_specific_replacement',
+      'do_not_replace'
+    ],
+    estimatedDeliveryTime: 69,
+    closed: true,
+    opensAt: '2018-05-31T09:00:00.000+08:00',
+    tags: [
+      {
+        id: 39,
+        title: '',
+        key: 'Coffee',
+        imageUrl: 'https://assets.honestbee.com/food/tags/coffee.jpg'
+      }
+    ],
+    minimumSpend: '15.0',
+    minimumSpendExtraFee: '30.99',
+    deliveryTypes: [
+      'asap',
+      'scheduled'
+    ],
+    shippingModes: [
+      'regular',
+      'offline'
+    ],
+    cashbackAmount: 0,
+    reservedTags: [
+      {
+        id: 101,
+        title: '',
+        key: 'habitat',
+        imageUrl: null
+      },
+      {
+        id: 95,
+        title: 'Eat More, Earn More',
+        key: 'cashback',
+        imageUrl: 'https://assets.honestbee.com/food/tags/foodtag-sumo6.jpg'
+      }
+    ],
+    minimumOrderFreeDelivery: '0.0',
+    defaultDeliveryFee: '0.0',
+    defaultConciergeFee: '0.0',
+    pricingType: 'no_pricing_policy',
+    brandTraits: []
+  },
+  {
+    id: 151,
+    name: 'Salad Stop',
+    slug: 'salad-stop',
+    description: 'Eat well and feel good',
+    about: null,
+    imageUrl: 'https://s3-ap-southeast-1.amazonaws.com/honestbees/headers/images/360x120/salad-stop.png',
+    productsImageUrl: 'https://s3-ap-southeast-1.amazonaws.com/honestbees/headers/images/360x80/product-list-salad-stop.png',
+    brandColor: '3b6143',
+    currency: 'SGD',
+    sameStorePrice: true,
+    brandType: 'regular',
+    promotionText: '',
+    parentBrandId: null,
+    productsCount: 31,
+    storeId: 178,
+    serviceType: 'food',
+    freeDeliveryEligible: true,
+    itemReplacementOptions: [
+      'find_best_match',
+      'pick_specific_replacement',
+      'do_not_replace'
+    ],
+    estimatedDeliveryTime: 5,
+    closed: true,
+    opensAt: '2018-05-31T09:00:00.000+08:00',
+    tags: [
+      {
+        id: 58,
+        title: '',
+        key: 'Japanese',
+        imageUrl: 'https://assets.honestbee.com/food/tags/best value.jpg'
+      },
+      {
+        id: 34,
+        title: 'Western',
+        key: 'Western',
+        imageUrl: 'https://assets.honestbee.com/food/tags/western.jpg'
+      },
+      {
+        id: 51,
+        title: 'Salads',
+        key: 'Salads',
+        imageUrl: 'https://assets.honestbee.com/food/tags/salads.jpg'
+      }
+    ],
+    minimumSpend: '15.0',
+    minimumSpendExtraFee: '30.99',
+    deliveryTypes: [
+      'asap',
+      'scheduled'
+    ],
+    shippingModes: [
+      'regular'
+    ],
+    cashbackAmount: 0,
+    reservedTags: [
+      {
+        id: 101,
+        title: '',
+        key: 'habitat',
+        imageUrl: null
+      },
+      {
+        id: 95,
+        title: 'Eat More, Earn More',
+        key: 'cashback',
+        imageUrl: 'https://assets.honestbee.com/food/tags/foodtag-sumo6.jpg'
+      }
+    ],
+    minimumOrderFreeDelivery: '100.0',
+    defaultDeliveryFee: '20.0',
+    defaultConciergeFee: '0.0',
+    pricingType: 'same_price_as_store',
+    brandTraits: [
+      {
+        slug: 'food',
+        name: 'Food',
+        header: 'Food',
+        imageUrlBasename: ''
+      }
+    ]
+  },
+  {
+    id: 452,
+    name: 'Sprout',
+    slug: 'sprout',
+    description: null,
+    about: null,
+    imageUrl: 'https://assets-staging.honestbee.com/headers/images/360x120/sprout.png',
+    productsImageUrl: '',
+    brandColor: '',
+    currency: 'SGD',
+    sameStorePrice: null,
+    brandType: 'regular',
+    promotionText: '',
+    parentBrandId: null,
+    productsCount: 58,
+    storeId: 594,
+    serviceType: 'food',
+    freeDeliveryEligible: true,
+    itemReplacementOptions: [
+      'find_best_match',
+      'pick_specific_replacement',
+      'do_not_replace'
+    ],
+    estimatedDeliveryTime: 60,
+    closed: true,
+    opensAt: '2018-05-31T11:00:00.000+08:00',
+    tags: [],
+    minimumSpend: '15.0',
+    minimumSpendExtraFee: '30.99',
+    deliveryTypes: [
+      'asap',
+      'scheduled'
+    ],
+    shippingModes: [
+      'regular'
+    ],
+    cashbackAmount: 0,
+    reservedTags: [
+      {
+        id: 101,
+        title: '',
+        key: 'habitat',
+        imageUrl: null
+      },
+      {
+        id: 95,
+        title: 'Eat More, Earn More',
+        key: 'cashback',
+        imageUrl: 'https://assets.honestbee.com/food/tags/foodtag-sumo6.jpg'
+      }
+    ],
+    minimumOrderFreeDelivery: '0.0',
+    defaultDeliveryFee: '0.0',
+    defaultConciergeFee: '0.0',
+    pricingType: 'no_pricing_policy',
+    brandTraits: []
+  },
+  {
+    id: 3267,
+    name: 'Rabbit Owl Depot @ Woodlands',
+    slug: 'rabbit-owl-depot--woodlands',
+    description: null,
+    about: null,
+    imageUrl: 'https://assets-staging.honestbee.com/headers/images/360x120/rabbit-owl-depot--woodlands.png',
+    productsImageUrl: null,
+    brandColor: null,
+    currency: 'SGD',
+    sameStorePrice: null,
+    brandType: 'regular',
+    promotionText: null,
+    parentBrandId: null,
+    productsCount: 29,
+    storeId: 4686,
+    serviceType: 'food',
+    freeDeliveryEligible: true,
+    itemReplacementOptions: [
+      'find_best_match',
+      'pick_specific_replacement',
+      'do_not_replace'
+    ],
+    estimatedDeliveryTime: 25,
+    closed: true,
+    opensAt: '2018-05-31T11:00:00.000+08:00',
+    tags: [],
+    minimumSpend: '15.0',
+    minimumSpendExtraFee: '30.99',
+    deliveryTypes: [
+      'asap',
+      'scheduled'
+    ],
+    shippingModes: [
+      'regular'
+    ],
+    cashbackAmount: 0,
+    reservedTags: [
+      {
+        id: 101,
+        title: '',
+        key: 'habitat',
+        imageUrl: null
+      },
+      {
+        id: 95,
+        title: 'Eat More, Earn More',
+        key: 'cashback',
+        imageUrl: 'https://assets.honestbee.com/food/tags/foodtag-sumo6.jpg'
+      }
+    ],
+    minimumOrderFreeDelivery: '0.0',
+    defaultDeliveryFee: '0.0',
+    defaultConciergeFee: '0.0',
+    pricingType: 'no_pricing_policy',
+    brandTraits: []
+  },
+  {
+    id: 440,
+    name: 'XO Noodles',
+    slug: 'xo-noodles',
+    description: null,
+    about: null,
+    imageUrl: 'https://assets-staging.honestbee.com/headers/images/360x120/xo-noodles.png',
+    productsImageUrl: '',
+    brandColor: '',
+    currency: 'SGD',
+    sameStorePrice: null,
+    brandType: 'regular',
+    promotionText: '',
+    parentBrandId: null,
+    productsCount: 6,
+    storeId: 570,
+    serviceType: 'food',
+    freeDeliveryEligible: true,
+    itemReplacementOptions: [
+      'find_best_match',
+      'pick_specific_replacement',
+      'do_not_replace'
+    ],
+    estimatedDeliveryTime: 40,
+    closed: true,
+    opensAt: '2018-05-31T11:00:00.000+08:00',
+    tags: [
+      {
+        id: 50,
+        title: 'Noodles',
+        key: 'Noodles',
+        imageUrl: 'https://assets.honestbee.com/food/tags/noodles.jpg'
+      }
+    ],
+    minimumSpend: '15.0',
+    minimumSpendExtraFee: '30.99',
+    deliveryTypes: [
+      'asap',
+      'scheduled'
+    ],
+    shippingModes: [
+      'regular'
+    ],
+    cashbackAmount: 6,
+    reservedTags: [
+      {
+        id: 101,
+        title: '',
+        key: 'habitat',
+        imageUrl: null
+      },
+      {
+        id: 95,
+        title: 'Eat More, Earn More',
+        key: 'cashback',
+        imageUrl: 'https://assets.honestbee.com/food/tags/foodtag-sumo6.jpg'
+      }
+    ],
+    minimumOrderFreeDelivery: '0.0',
+    defaultDeliveryFee: '0.0',
+    defaultConciergeFee: '0.0',
+    pricingType: 'no_pricing_policy',
+    brandTraits: []
+  },
+  {
+    id: 453,
+    name: 'Ramen BOWL',
+    slug: 'ramen-bowl',
+    description: null,
+    about: null,
+    imageUrl: 'https://assets-staging.honestbee.com/headers/images/360x120/ramen-bowl.png',
+    productsImageUrl: '',
+    brandColor: '',
+    currency: 'SGD',
+    sameStorePrice: null,
+    brandType: 'regular',
+    promotionText: '',
+    parentBrandId: null,
+    productsCount: 15,
+    storeId: 595,
+    serviceType: 'food',
+    freeDeliveryEligible: true,
+    itemReplacementOptions: [
+      'find_best_match',
+      'pick_specific_replacement',
+      'do_not_replace'
+    ],
+    estimatedDeliveryTime: 20,
+    closed: true,
+    opensAt: '2018-05-31T11:00:00.000+08:00',
+    tags: [
+      {
+        id: 54,
+        title: '',
+        key: 'Food',
+        imageUrl: null
+      },
+      {
+        id: 58,
+        title: '',
+        key: 'Japanese',
+        imageUrl: 'https://assets.honestbee.com/food/tags/best value.jpg'
+      }
+    ],
+    minimumSpend: '15.0',
+    minimumSpendExtraFee: '30.99',
+    deliveryTypes: [
+      'asap',
+      'scheduled'
+    ],
+    shippingModes: [
+      'regular',
+      'merchant_delivery'
+    ],
+    cashbackAmount: 0,
+    reservedTags: [
+      {
+        id: 101,
+        title: '',
+        key: 'habitat',
+        imageUrl: null
+      },
+      {
+        id: 95,
+        title: 'Eat More, Earn More',
+        key: 'cashback',
+        imageUrl: 'https://assets.honestbee.com/food/tags/foodtag-sumo6.jpg'
+      }
+    ],
+    minimumOrderFreeDelivery: '0.0',
+    defaultDeliveryFee: '0.0',
+    defaultConciergeFee: '0.0',
+    pricingType: 'no_pricing_policy',
+    brandTraits: []
+  }
+];
+
