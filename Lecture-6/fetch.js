d75ab0b986036c260535379c3687b382ee11f8e7
EthanChou <ethan.chou@honestbee.com>
2018-06-29 14:35:30 +0800

    remove lecture 4 async demo code

--- Lecture-6/fetch.js
+++
@@ -1,5 +0,0 @@
-// should return 5 random users
-fetch('https://randomuser.me/api/?results=5')
-.then(response => response.json())
-.then(json => console.log(json))
-.catch(err => console.log('err: ', err))
